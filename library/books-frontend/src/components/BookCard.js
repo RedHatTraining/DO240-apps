588132f058e31c47e0a278fc2d4188c243d6bac7
Marek Czernek <mczernek@redhat.com>
2022-02-28 14:16:28 +0100

    Implement peer review

--- library/books-frontend/src/components/BookCard.js
+++ library/books-frontend/src/components/BookCard.js
@@ -1,40 +1,26 @@
 import { Card, CardBody, CardExpandableContent, CardHeader, TextContent } from "@patternfly/react-core";
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class BookCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isExpanded: false,
-    };
-    this.onExpand = () => {
-      this.setState({
-        isExpanded: !this.state.isExpanded
-      });
-    };
-  }
-
-  render(){
-    const { year, copies, title } = this.props;
-    // const { authorName } = this.props;
-    const { isExpanded } = this.state;
-    return (
-      <>
-        <Card style={{margin: "20px"}} isExpanded={isExpanded} >
-          <CardHeader onExpand={this.onExpand}>
-            { title }
-          </CardHeader>
-          <CardExpandableContent>
-            <CardBody>
-              <TextContent>
-                {/* <p>Author: {authorName}</p> */}
-                <p>Year of publishing: {year}</p>
-                <p>Copies available: {copies}</p>
-              </TextContent>
-            </CardBody>
-          </CardExpandableContent>
-        </Card>
-      </>
-    );
-  }
+export default function BookCard(props) {
+  const { year, copies, title, author } = props;
+  const [isExpanded, setIsExpanded] = useState(false);
+  
+  return (
+    <>
+      <Card style={{margin: "20px"}} isExpanded={isExpanded} >
+        <CardHeader onExpand={() => setIsExpanded(!isExpanded)}>
+          { title }
+        </CardHeader>
+        <CardExpandableContent>
+          <CardBody>
+            <TextContent>
+              <p>Author: {author.authorName}</p>
+              <p>Year of publishing: {year}</p>
+              <p>Copies available: {copies}</p>
+            </TextContent>
+          </CardBody>
+        </CardExpandableContent>
+      </Card>
+    </>
+  );
 }
--- library/books-frontend/src/services/keycloak.js
+++ library/books-frontend/src/services/keycloak.js
@@ -3,7 +3,6 @@ import Keycloak from "keycloak-js";
 const url = process.env.REACT_APP_KEYCLOAK_URL || "http://localhost:8080/auth";
 const realm = process.env.REACT_APP_KEYCLOAK_REALM || "ad240-realm";
 const clientId = process.env.REACT_APP_CLIENT_ID || "react-auth";
-// const clientSecret = process.env.REACT_APP_CLIENT_SECRET || "RkjZ5kyRcC5mSajZ6ShZjs2MYQ48tLiu";
 
 const keycloak = new Keycloak({
   url,
