90a2924cf6b26291ee376d9f6fa0ad25174be26f
Alex <alex.serna@redhat.com>
2022-03-05 09:39:22 +0100

    add namespace to oc commands in email script (#8)

--- scripts/get-emails.sh
+++ scripts/get-emails.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-POD_NAME=$(oc get pods --selector=app=smtp -o=jsonpath='{.items[0].metadata.name}')
-POD_LOGS=$(oc logs $POD_NAME)
+T_SCALE_PROJECT="3scale"
+POD_NAME=$(oc get pods --selector=app=smtp -o=jsonpath='{.items[0].metadata.name}' -n $T_SCALE_PROJECT)
+POD_LOGS=$(oc logs $POD_NAME -n $T_SCALE_PROJECT)
 
 echo "$POD_LOGS"
