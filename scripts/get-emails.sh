23d12c5f74d7056a8d3731900b9644921d6e5240
Marek Czernek <mczernek@redhat.com>
2022-03-22 14:32:29 +0100

    Fix: Include login to OCP (#9)

--- scripts/get-emails.sh
+++ scripts/get-emails.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 T_SCALE_PROJECT="3scale"
+OCP_USER="admin"
+OCP_PASS="redhat"
+OCP_URL="https://api.ocp4.example.com:6443"
+
+oc login -u "${OCP_USER}" -p "${OCP_PASS}" "${OCP_URL}"
 POD_NAME=$(oc get pods --selector=app=smtp -o=jsonpath='{.items[0].metadata.name}' -n $T_SCALE_PROJECT)
 POD_LOGS=$(oc logs $POD_NAME -n $T_SCALE_PROJECT)
 
